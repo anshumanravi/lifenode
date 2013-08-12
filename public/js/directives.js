ff00bab2d10b9c27e3e2cdf9aa26e944ee27b303
Anshuman Mishra <ravianshmsr08@gmail.com>
2013-08-12 20:31:35 +0530

    Added nodeCircle directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,9 +3,38 @@
 /* Directives */
 
 
-angular.module('lifenode.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('lifenode.directives', [])
+	.directive('appVersion', ['version', function(version) {
+		return function(scope, elm, attrs) {
+			elm.text(version);
+		};
+	}])
+
+	
+	.directive('nodeCircle', function(){
+		return {
+			scope: {
+				clickCallback: '&',
+				options: '='
+			},
+			restrict: 'E',
+			link: function (scope, element){
+				element.bind('mouseenter',function() {
+					console.log('MOUSE ENTER: ' );
+				});
+				element.bind('mouseleave',function() {
+					console.log('LEAVE');
+				});
+			},
+			template:
+			'<div class="circle">' +
+		'<div class="outer-circle"></div>' +
+		'<div class="middle-circle"></div>'+
+		'<div class="inner-circle">lifenode</div>' +
+		'</div>',
+		};
+	});
+
+
+
+	
